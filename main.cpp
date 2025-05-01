ba6ac33e9f50457b44a4851ff207fa4310077767
katuninaLisa <licatu@yandex.ru>
2025-05-01 19:36:27 +0300

    Added function create_matrix

--- main.cpp
+++ main.cpp
@@ -1,4 +1,25 @@
 #include <QCoreApplication>
+#include "class.h"
+#include "property.h"
+
+void createMatrix(QMap <QString, QMap<QString,int>> &matrix, const QSet <Class> &classes)
+{
+    QStringList classNames;
+    for (const Class &rec :classes)
+    {
+        classNames.append(rec.getName());
+    }
+
+    for (const Class &rec : classes)
+    {
+        QMap<QString,int> row;
+        for (const QString col : classNames)
+        {
+            row[col] = 0;
+        }
+        matrix[rec.getName()] = row;
+    }
+}
 
 int main(int argc, char *argv[])
 {
